5ebde15a060b39ef52f737718ac019e98e93e325
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2023-04-26 13:31:59 +0200

    serialize_and_write_pair()

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #define INPUT_FILE_NAME "data_in.txt"
 #define OUTPUT_FILE_NAME "data_out.txt"
@@ -49,59 +51,113 @@ typedef struct {
 typedef struct {
     input_msg_t input_msg;
     uint32_t    mask;
-} pair_t;
+} __attribute__((packed)) pair_t;
 
 
 int main(){
 
     int ret = 0;
 
-    FILE *fd_out = fopen(OUTPUT_FILE_NAME, "rw");
-    if (fd_out == NULL) {
-        printf("Error opening %s file\n", OUTPUT_FILE_NAME);
-        return 1;
-    }
+    pair_t pair = {
+        .input_msg = {
+            .type = 1,
+            .length = 255,
+            .payload[0] = 0,
+            .data[0] = 0,
+            .crc32 = 0x12345678,
+        },
+        .mask = 0xDEADBEEF
+    };
 
-    FILE *fd_in = fopen(INPUT_FILE_NAME, "r");
-    if (fd_in == NULL) {
-        fprintf(fd_out, "Error opening %s file\n", INPUT_FILE_NAME);
-        return 1;
-    }
+    memset(&pair.input_msg.payload, 0, sizeof(pair.input_msg.payload));
+    memset(&pair.input_msg.data, 0, sizeof(pair.input_msg.data));
 
-    pair_t *pairs = NULL;
-    size_t pairs_num = 0;
+    serialize_and_write_pair("pair", &pair);
 
-    ret = extract_pairs(fd_in, pairs, &pairs_num);
-    if(ret != 0){
-        fprintf(fd_out, "Pairs extracting error\n");
-        return 1;
-    }
+    // FILE *fd_out = fopen(OUTPUT_FILE_NAME, "a");
+    // if (fd_out == NULL) {
+    //     printf("Error opening %s file\n", OUTPUT_FILE_NAME);
+    //     return 1;
+    // }
+
+    // FILE *fd_in = fopen(INPUT_FILE_NAME, "r");
+    // if (fd_in == NULL) {
+    //     fprintf(fd_out, "Error opening %s file\n", INPUT_FILE_NAME);
+    //     return 1;
+    // }
+
+    // pair_t *pairs = NULL;
+    // size_t pairs_num = 0;
+
+    // ret = extract_pairs(fd_in, pairs, &pairs_num);
+    // if(ret != 0){
+    //     fprintf(fd_out, "Pairs extracting error\n");
+    //     return 1;
+    // }
     
-    output_msg_t *output = NULL;
-    size_t output_num = 0;
+    // output_msg_t *output = NULL;
+    // size_t output_num = 0;
 
-    ret = process_pairs(pairs, pairs_num, output, &output_num);
-    if(ret != 0){
-        fprintf(fd_out, "Pairs processing error\n");
-        return 1;
-    }
+    // ret = process_pairs(pairs, pairs_num, output, &output_num);
+    // if(ret != 0){
+    //     fprintf(fd_out, "Pairs processing error\n");
+    //     return 1;
+    // }
 
-    ret = save_outputs(fd_out, output, output_num);
-    if(ret != 0){
-        fprintf(fd_out, "Saving outputs error\n");
-        return 1;
-    }
+    // ret = save_outputs(fd_out, output, output_num);
+    // if(ret != 0){
+    //     fprintf(fd_out, "Saving outputs error\n");
+    //     return 1;
+    // }
 
     return 0;
 }
 
-// if no errors and pairs_num != 0
-// pairs will point to the pairs array
+//
 int extract_pairs(FILE *fd, pair_t *pairs, size_t *pairs_num){
+
     return 0;
 }
 
-int process_pairs(pair_t *pairs, size_t *pairs_num, output_msg_t *out, size_t *output_msg_num){
+//
+int serialize_and_write_pair(const char *filename, pair_t *pair){
+
+    if(filename == NULL || pair == NULL){
+        return 1;
+    }
+
+    FILE *fd = fopen(filename, "w+");
+    if (fd == NULL) {
+        return 1;
+    }
+
+    fprintf(fd, "%s", MSG_PREFIX);
+
+    size_t written = fwrite(&pair->input_msg, sizeof(char), sizeof(input_msg_t), fd);
+    if(written != sizeof(input_msg_t)){
+        fclose(fd);
+        return 1;
+    }
+
+    fprintf(fd, "%s", MASK_PREFIX);
+
+    written = fwrite(&pair->mask, sizeof(char), sizeof(pair->mask), fd);
+    if(written != sizeof(pair->mask)){
+        fclose(fd);
+        return 1;
+    }
+
+    fclose(fd);
+
+    return 0;
+}
+
+int make_pair(pair_t *out){
+    
+}
+
+//
+int process_pairs(pair_t *pairs, size_t pairs_num, output_msg_t *out, size_t *output_msg_num){
     
     if(pairs == NULL) {
         return 1;
@@ -114,6 +170,7 @@ int process_pairs(pair_t *pairs, size_t *pairs_num, output_msg_t *out, size_t *o
     return 0;
 }
 
+//
 int save_outputs(FILE *fd_out, output_msg_t *out_msgs, size_t out_msgs_num){
     return 0;
 }
